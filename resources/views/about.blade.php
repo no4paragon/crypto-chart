5aca1fb6272b180cdbe380c0c9407be9d0e45f67
no4paragon <point_up@hotmail.com>
2021-12-08 19:50:21 -0800

    ccc

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="container-lg" style="margin: 0 auto;">
-    <h2 class = "tex-center mt-3" style ="font-family: 'Courier New', Courier, monospace; color:blue; font-wight:bold">Coin charts</h2>
+    <h2 class = "tex-center mt-3" style ="font-family: 'Courier New', Courier, monospace; color:blue; font-weight:bold">Coin charts</h2>
     <p class = "text-center mt-2">This shows crypt charts</p>
 
 </div>
